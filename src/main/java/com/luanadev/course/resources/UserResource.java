0f3a51b9b81abb647458b992e0170aa1cd9b3e8b
Luana Lima <luanalima.developer@gmail.com>
2022-01-25 00:51:59 -0300

    User entity and resource

---
+++ src/main/java/com/luanadev/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.luanadev.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luanadev.course.entities.User;
+
+
+@RestController /* recurso web implementado por um controlador rest */
+@RequestMapping(value = "/users")  /* nome do recurso */
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "5564999999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
