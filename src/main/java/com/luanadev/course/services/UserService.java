6683e80ebd97ad1eb513bb6943ffca390868aad9
luanalimadeveloper <luanalima.developer@gmail.com>
2022-02-14 12:18:44 -0300

    Service layer, component registration

---
+++ src/main/java/com/luanadev/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.luanadev.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luanadev.course.entities.User;
+import com.luanadev.course.repositories.UserRepository;
+
+@Service /*Resgistra como um componente - Classe do Spring que pode ser injetada automaticamente com Autowired*/
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj =  repository.findById(id);
+		return obj.get();
+	}
+
+}
