5772cbc01044984e3bf022dd63c9c2af0a6786eb
luanalimadeveloper <luanalima.developer@gmail.com>
2022-02-15 17:42:27 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/luanadev/course/entities/User.java
+++ src/main/java/com/luanadev/course/entities/User.java
@@ -1,14 +1,19 @@
 package com.luanadev.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 /* Implements Serializable - Para ser transformado para cadeia de bytes para trafegar na rede */
 @Entity
+@Table(name="tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -19,6 +24,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -71,6 +79,10 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	/* Para comparar um objeto com outro (nesse caso somente é usado o id) */
 	@Override
@@ -97,6 +109,8 @@ public class User implements Serializable{
 			return false;
 		return true;
 	}
+
+
 	
 	
 	
